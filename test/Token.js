6aa1d0fc83810942cbd72f57c4da7cfa59ffabd6
Andreas Anastasiades <andreasanastasiades@MacBook-Pro-2.local>
2024-01-23 19:48:40 +0200

    6. Token appprovals

--- test/Token.js
+++ test/Token.js
@@ -6,7 +6,7 @@ const tokens = (n) => {
 }
 
 describe('Token', ()=> {
-    let token, accounts, deployer, receiver
+    let token, accounts, deployer, receiver, exchange
 
     describe('Success', () =>{
         beforeEach( async () =>{
@@ -17,7 +17,7 @@ describe('Token', ()=> {
             accounts = await ethers.getSigners()
             deployer = accounts[0]
             receiver = accounts[1]
-    
+            exchange = accounts[2]
         })
     
         describe('Deployment', () => {
@@ -97,6 +97,36 @@ describe('Token', ()=> {
                 amount)).to.be.reverted
         })
 
+    })
+
+    describe('Token Approvals', () =>{   
+        let amount, transaction, result
+        beforeEach(async () => {
+            amount = tokens(100)
+            transaction = await token.connect(deployer).approve(exchange.address, amount)
+            result = await transaction.wait()
+        })
+        describe('Success', () =>{   
+            it('allows an allowance for delegated token supply spending', async ()=>{
+                expect(await token.allowance(deployer.address, exchange.address)).to.equal(amount)
+            })
+
+            it('emits an approval event', async () => {
+                const event = result.events[0]
+                expect(event.event).to.equal('Approval')  
+        
+                const args = event.args 
+                expect(args.owner).to.equal(deployer.address)  
+                expect(args.spender).to.equal(exchange.address)  
+                expect(args.value).to.equal(amount)  
+                })
+        })
+        describe('Failure', () =>{  
+                it('rejects invalid spenders', async() => {
+                    await expect(token.connect(deployer).approve('0x0000000000000000000000000000000000000000', 
+                    amount)).to.be.reverted
+                })
+        })
     })
 
 })
