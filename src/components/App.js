4bf12eb50c73677720eef975b3a4a111ed39c887
Andreas Anastasiades <andreasanastasiades@MacBook-Pro-2.local>
2024-02-18 14:32:16 +0200

    19. Select markets

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import  { useDispatch } from 'react-redux'
 import config from '../config.json'
 import { loadProvider, loadNetwork, loadAccount, loadTokens, loadExchange } from '../store/interactions';
 import Navbar from './Navbar'
+import Markets from './Markets';
 
 function App() {
 
@@ -48,7 +49,7 @@ const loadBlockchainData = async () => {
       <main className='exchange grid'>
         <section className='exchange__section--left grid'>
 
-          {/* Markets */}
+        <Markets />
 
           {/* Balance */}
 
